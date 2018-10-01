2673d6cb24fc3f8ae1d5e83d34c260ae58a7f5b5
ShriyaThangada <43002669+ShriyaThangada@users.noreply.github.com>
2018-10-01 08:33:02 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,20 @@
+
+public class OperationsPractice {
+public static void main(String[] args) 
+{
+	int number; 
+	number = 20;
+	number = number + 5;
+	System.out.println("Add 5:"+ number);
+	number= number/2;
+		System.out.println(number);	
+		
+	number*=4;
+	System.out.println(number);
+	number-=6;
+	System.out.println(number);
+	number%=2;
+	System.out.println(number);
+			
+}
+}
